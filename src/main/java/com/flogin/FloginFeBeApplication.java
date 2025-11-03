ae8b91896079f0fd9d56723492a02be212291d49
KhanggCloud <vuhongvinhkhang@gmail.com>
2025-11-03 19:44:56 +0700

    Update: Project Structure Part 1

---
+++ src/main/java/com/flogin/FloginFeBeApplication.java
@@ -0,0 +1,13 @@
+package com.flogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloginFeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloginFeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flogin/FloginFeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloginFeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
