92162784b42b5635a7f3162fc151bf00796ca341
KhanggCloud <vuhongvinhkhang@gmail.com>
2025-11-29 16:50:44 +0700

    Fix CI for Product

--- frontend/src/cypress.config.js
+++ frontend/src/cypress.config.js
@@ -2,10 +2,10 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        supportFile: "cypress/support/e2e.js",
+        supportFile: "src/cypress/support/e2e.js",
 
         // 2. Sửa đường dẫn specPattern (Để nó tìm thấy file test)
-        specPattern: "cypress/e2e/*.cy.{js,jsx,ts,tsx}",
+        specPattern: "src/cypress/e2e/*.cy.{js,jsx,ts,tsx}",
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
