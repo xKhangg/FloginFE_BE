21286826c0fdb32134794e891b5cd8485041fe68
KhanggCloud <vuhongvinhkhang@gmail.com>
2025-11-30 19:24:45 +0700

    Fix Cypress Config

--- frontend/src/cypress.config.js
+++ frontend/src/cypress.config.js
@@ -2,7 +2,7 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        supportFile: "./cypress/support/e2e.js",
+        supportFile: false,
 
         specPattern: "./cypress/e2e/*.cy.{js,jsx,ts,tsx}",
         setupNodeEvents(on, config) {
